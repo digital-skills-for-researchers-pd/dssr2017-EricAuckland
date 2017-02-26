c269c48df2fa8a930922720dc1774bd70cc3bc01
CoolDude <eros030@aucklanduni.ac.nz>
2017-02-27 11:46:58 +1300

    working scalp locations file

--- egiScalpLoc.m
+++ egiScalpLoc.m
@@ -1,3 +1,5 @@
 % Make sure egi file is in data folder
 % Apply EGI scalp locations
-EEG=pop_chanedit(EEG, 'load',{strcat(pathname, 'GSN-HydroCel-129.sfp'), 'filetype' 'autodetect'},'setref',{'4:132' 'Cz'},'changefield',{132 'datachan' 0});
+function [EEG] = egiScalpLoc(EEG, pathName) 
+EEG=pop_chanedit(EEG, 'load',{strcat(pathName, 'GSN-HydroCel-129.sfp'), 'filetype' 'autodetect'},'setref',{'4:132' 'Cz'},'changefield',{132 'datachan' 0});
+end
