f6718f365f8ff7615ee6d9d24780c6dd6903ed19
CoolDude <eros030@aucklanduni.ac.nz>
2017-02-27 12:03:42 +1300

    filter function now working

--- filterPPP.m
+++ filterPPP.m
@@ -1,3 +1,4 @@
 function [EEG] = filterPPP(EEG)
 % Filter the dataset
 EEG  = pop_basicfilter( EEG,  1:128 , 'Boundary', 'boundary', 'Cutoff',  [ 0.1 30], 'Design', 'butter', 'Filter', 'bandpass', 'Order',  4 );
+end
