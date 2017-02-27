6e371a7ee921dd0d8d98af835cb2bf12f5ac6d4a
CoolDude <eros030@aucklanduni.ac.nz>
2017-02-28 12:24:22 +1300

    can add two or more triggers

--- epochPPP.m
+++ epochPPP.m
@@ -1,5 +1,7 @@
 function [EEG] = epochPPP(EEG, paraMinEpoch, paraMaxEpoch, paraTriggers)
 
-EEG = pop_epoch( EEG, {paraTriggers}, [(paraMinEpoch/1000), (paraMaxEpoch/1000)], 'newname', paraTriggers, 'epochinfo', 'yes');
+splitParaTriggers = strsplit(paraTriggers,' ')
+
+EEG = pop_epoch( EEG, (splitParaTriggers), [(paraMinEpoch/1000), (paraMaxEpoch/1000)], 'newname', paraTriggers, 'epochinfo', 'yes');
 
 end
