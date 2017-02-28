4fe52de636f4db8a324c21d4c215eb916692b6de
CoolDude <eros030@aucklanduni.ac.nz>
2017-03-01 10:19:05 +1300

    renamed by adding PPP

---
+++ correctLatencyPPP.m
@@ -0,0 +1,9 @@
+function [EEG] = correctLatency(EEG, delaySize)
+
+% Corrects for the amplifier and stimulus presentation delay
+samplingRateFix = 1000/EEG.srate;
+adjustedDelaySize = delaySize/samplingRateFix;
+
+for j = 1:size(EEG.event,2);
+    EEG.event(j).latency = EEG.event(j).latency + adjustedDelaySize;
+end;
