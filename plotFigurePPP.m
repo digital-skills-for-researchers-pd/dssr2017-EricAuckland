3b0eb333e81e4dc39ec05657f814843fc8be74ed
CoolDude <eros030@aucklanduni.ac.nz>
2017-02-28 14:02:19 +1300

    will now plot chosen electrodes

--- plotFigurePPP.m
+++ plotFigurePPP.m
@@ -1,6 +1,5 @@
-function [ EEG ] = plotFigurePPP(EEG, paraElectrodes, paraTriggers);
+function [ EEG ] = plotFigurePPP(EEG, paraElectrodes);
 
-figure; pop_erpimage(EEG,1, [paraElectrodes],[[]],'title',10,1,{paraTriggers},[],'','yerplabel','\muV','erp','on','cbar','on','topo', { [paraElectrodes] EEG.chanlocs EEG.chaninfo } );
+figure; pop_erpimage(EEG,1, paraElectrodes,[[]],(num2str(paraElectrodes)),10,1,{},[],'' ,'yerplabel','\muV','erp','on');
 
 end
-
