745f82ca7eeddd68608412e2a5057cc0418ce363
CoolDude <eros030@aucklanduni.ac.nz>
2017-03-01 10:19:44 +1300

    renamed file

--- correctLatency.m
+++
@@ -1,9 +0,0 @@
-function [EEG] = correctLatency(EEG, delaySize)
-
-% Corrects for the amplifier and stimulus presentation delay
-samplingRateFix = 1000/EEG.srate;
-adjustedDelaySize = delaySize/samplingRateFix;
-
-for j = 1:size(EEG.event,2);
-    EEG.event(j).latency = EEG.event(j).latency + adjustedDelaySize;
-end;
