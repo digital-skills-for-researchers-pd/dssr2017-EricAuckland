0a55652c684855b48edb62adb600bbb7c91c1104
CoolDude <eros030@aucklanduni.ac.nz>
2017-03-02 09:39:53 +1300

    added in eg stuff

--- parametersPPP.m
+++ parametersPPP.m
@@ -1,7 +1,7 @@
 function [paraElectrodes, paraTriggers, delaySize, paraMinEpoch, paraMaxEpoch] = parametersPPP()
 
 % Parameter questions
-prompt={'What electrode/s do you desire:','Name triggers to epoch around:','What is the trigger latency and amplifier lag (ms)?:','Epoch start (ms):','Epoch end (ms):'};
+prompt={'What electrode/s do you desire (e.g. 58):','Name triggers to epoch around (e.g. Dist1):','What is the trigger latency and amplifier lag (ms)?:','Epoch start (ms):','Epoch end (ms):'};
 title='ParametersPPP'; 
 
 % The answers are converted to variables and usual types
